e490334759c554f5949c6362f40014ba6780acc8
issei_ezawa <issemann@yahoo.co.jp>
2022-07-16 19:27:36 +0900

    Add: スタートとストップ時にボタンが切り替わるように設定
    buttonの形を柔らかそうな形にしました

--- app/javascript/components/TheRoulette.vue
+++ app/javascript/components/TheRoulette.vue
@@ -7,11 +7,8 @@
       <div class="mx-auto text-center">
         <transition name="fade">
           <div>
-            <button @click="startLoop(100)">
-              おす
-            </button>
-            <button @click="slowLoop()">
-              おす
+            <button @click="startOrStop ? startLoop(100) : slowLoop()" class="btn btn-pink-moon">
+              {{ startOrStopButton }}
             </button>
             <p>{{ pickedUpWords }}</p>
           </div>
@@ -31,12 +28,16 @@ export default {
   },
   data() {
     return {
-      intervId: null
+      intervId: null,
+      startOrStop: true
     }
   },
   computed: {
     ...mapGetters('selectedWords', ['selectedWords']),
-    ...mapGetters('randomPickedUp', ['pickedUpWords'])
+    ...mapGetters('randomPickedUp', ['pickedUpWords']),
+    startOrStopButton() {
+      return this.startOrStop ? 'スタート' : 'ストップ'
+    }
   },
   methods: {
     ...mapActions('randomPickedUp', ['randomPickedUpNumbers']),
@@ -46,6 +47,7 @@ export default {
                                     time,
                                     this.selectedWords
                                     )
+        this.startOrStop = false
       }
     },
     slowLoop() {
@@ -57,6 +59,7 @@ export default {
     stopLoop() {
       clearInterval(this.intervId)
       this.intervId = null
+      this.startOrStop = true
     }
   }
 }
@@ -65,5 +68,17 @@ export default {
 <style scoped>
 .container {
   background: radial-gradient(#ffffff, #fde9ff);
+}
+.btn-pink-moon {
+    background: #ff8ad0;
+    background: -webkit-linear-gradient(to top, #fc7a7a, #fc81cb);
+    background: linear-gradient(to top, #fc7a7a, #fc81cb);
+    color: #fff;
+    border: 3px solid #eee;
+    border-radius: 15px;
+    transition: 0.3s;
+}
+.btn-pink-moon:hover {
+  color: #ff0000;
 }
 </style>
