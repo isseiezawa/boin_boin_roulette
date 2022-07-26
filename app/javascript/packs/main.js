85137b8981cb8b8a5b82e34008d7da440b68ed8f
issei_ezawa <issemann@yahoo.co.jp>
2022-07-27 02:45:37 +0900

    Add: fontawesomeインストールと設定追加

--- app/javascript/packs/main.js
+++ app/javascript/packs/main.js
@@ -6,6 +6,13 @@ import axios from '../plugins/axios'
 import 'bootstrap/dist/css/bootstrap.css'
 // ドロップダウン使用の為追加
 import "bootstrap/dist/js/bootstrap.bundle.min.js"
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCirclePlay } from '@fortawesome/free-regular-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faCirclePlay)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.prototype.$axios = axios
 
