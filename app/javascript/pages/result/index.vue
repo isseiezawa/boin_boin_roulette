b17bbc3b2601c6a02bbbd0fab9bf838fe50c28ae
issei_ezawa <issemann@yahoo.co.jp>
2022-07-27 01:21:43 +0900

    Add: 集めたワードのページにスピーカー機能追加

--- app/javascript/pages/result/index.vue
+++ app/javascript/pages/result/index.vue
@@ -3,24 +3,49 @@
     <h2 class="text-center mb-4">集めたワード</h2>
     <div class="row row-cols-4">
       <div v-for="(resultWord, index) in resultWords" :key="index" class="col border">
-        {{ resultWord.word }}
+        <div>{{ resultWord.word }}<button @click="getVoice(resultWord.word)">再生</button></div>
       </div>
     </div>
+    <speech-setting-box class="mt-3" />
   </div>
 </template>
 
 <script>
+import SpeechSettingBox from '../../components/SpeechSettingBox.vue'
 import { mapGetters, mapActions } from "vuex"
 
 export default {
   created() {
-    this.fetchResultWords()
+    this.fetchResultWords();
+    speechSynthesis.getVoices();
+  },
+  components: {
+    SpeechSettingBox
   },
   computed: {
-    ...mapGetters("randomPickedUp", ["resultWords"])
+    ...mapGetters("randomPickedUp", ["resultWords"]),
+    ...mapGetters("voiceSetting", [
+      "selectVoice",
+      "volumeLevel",
+      "speedSetting",
+      "pitchSetting",
+    ]),
   },
   methods: {
-    ...mapActions("randomPickedUp", ["fetchResultWords"])
+    ...mapActions("randomPickedUp", ["fetchResultWords"]),
+    ...mapActions("voiceSetting", ["setVoiceList"]),
+    getVoice(word) {
+    const speechOption = new SpeechSynthesisUtterance();
+    const voice = speechSynthesis.getVoices();
+    this.setVoiceList(voice);
+    speechOption.voice = voice[this.selectVoice];
+    speechOption.volume = this.volumeLevel;
+    speechOption.rate = this.speedSetting;
+    speechOption.pitch = this.pitchSetting;
+    speechOption.text = word;
+    speechSynthesis.cancel();
+    speechSynthesis.speak(speechOption);
+    }
   }
 }
 </script>
