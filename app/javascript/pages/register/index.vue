0c287671e28e6c7ec6a5b50c51da67d96efab817
issei_ezawa <issemann@yahoo.co.jp>
2022-07-28 21:18:00 +0900

    Add: users のstore作成(認証されたユーザーの状態保存) vue側のroute設定
    画面にルーティングされる前にパラメータのバリデーションをして、アクセスのガードをする設定追加(router.beforeEach)。

---
+++ app/javascript/pages/register/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>ユーザー登録</h1>
+  </div>
+</template>
--- app/javascript/router/index.js
+++ app/javascript/router/index.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import TopIndex from '../pages/top/index'
 import ResultIndex from '../pages/result/index'
+import RegisterIndex from '../pages/register/index'
+import LoginIndex from '../pages/login/index'
+import store from '../store'
 
 Vue.use(Router)
 
@@ -16,9 +19,30 @@ const router = new Router({
     {
       path: "/results",
       component: ResultIndex,
-      name: "ResultIndex"
+      name: "ResultIndex",
+      meta: { requiredAuth: true }
+    },
+    {
+      path: "/register",
+      component: RegisterIndex,
+      name: "RegisterIndex"
+    },
+    {
+      path: '/login',
+      component: LoginIndex,
+      name: 'LoginIndex'
     }
   ]
+})
+
+router.beforeEach((to, from, next) => {
+  store.dispatch('users/fetchAuthUser').then((authUser) => {
+    if (to.matched.some(record => record.meta.requiredAuth) && !authUser) {
+      next({ name: 'LoginIndex' });
+    } else {
+      next();
+    }
+  })
 })
 
 export default router
