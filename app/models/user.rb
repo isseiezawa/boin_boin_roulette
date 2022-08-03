b067dc7ba9c484d11eb0a481eeb0e912eb7bd781
issei_ezawa <issemann@yahoo.co.jp>
2022-08-03 21:30:21 +0900

    Add: settingモデル追加。userとの紐付け

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   authenticates_with_sorcery!
 
   has_many :results, dependent: :destroy
+  has_one :setting, dependent: :destroy
 
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
