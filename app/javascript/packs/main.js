fcc388babb89b83b1ca4a6af65616e3de7640ae2
issei_ezawa <issemann@yahoo.co.jp>
2022-08-31 22:25:51 +0900

    Add: twitterのロゴマークを使用するためのパッケージインストール

--- app/javascript/packs/main.js
+++ app/javascript/packs/main.js
@@ -10,9 +10,10 @@ import 'bootstrap/dist/css/bootstrap.css'
 import "bootstrap/dist/js/bootstrap.bundle.min.js"
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faCirclePlay, faUser, faAt, faKey  } from '@fortawesome/free-solid-svg-icons'
+import { faTwitter } from "@fortawesome/free-brands-svg-icons"
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-library.add(faCirclePlay, faUser, faAt, faKey)
+library.add(faCirclePlay, faUser, faAt, faKey, faTwitter)
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
